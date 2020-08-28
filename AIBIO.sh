4a56a1f6ca02678f0349b2894fbc06c955dc00b5
MuLan <70324862+JungleWarriorMuLan@users.noreply.github.com>
2020-08-28 13:45:28 +0200

    Create AIBIO.sh

---
+++ AIBIO.sh
@@ -0,0 +1,9 @@
+#!/bin/sbin/python
+
+#
+# auto create Coder
+#
+
+Echo "Hello, welcome to Space-Time Drive."
+echo "Here you can find your favorite books and read interesting topics in ZenHouse."
+echo "Yes. You need to write down important content and share your thoughts with the subject people in ZenHouse."
